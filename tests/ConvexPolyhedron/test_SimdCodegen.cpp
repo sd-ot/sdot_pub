6e14f071b689c2cbb46ecd58b11b12f88a1f17aa
Hugo Leclerc <hugal.leclerc@gmail.com>
2019-12-26 14:20:30 +0100

    tst

--- tests/ConvexPolyhedron/test_SimdCodegen.cpp
+++ tests/ConvexPolyhedron/test_SimdCodegen.cpp
@@ -5,12 +5,29 @@
 int main( int /*argc*/, char **/*argv*/ ) {
     SimdGraph gr;
 
-    gr.add_target(
-        gr.make_op( "+", {
-            gr.make_op( "R0", {} ),
-            gr.make_op( "R1", {} ),
-        } )
-    );
+    SimdOp *px_0 = gr.make_op( "REG px_0 d 4", {} );
+    SimdOp *px_a = gr.make_op( "GET 0", { px_0 } );
+    SimdOp *px_b = gr.make_op( "GET 1", { px_0 } );
+
+    SimdOp *di_0 = gr.make_op( "REG di_0 d 4", {} );
+    SimdOp *di_a = gr.make_op( "GET 0", { di_0 } );
+    SimdOp *di_b = gr.make_op( "GET 1", { di_0 } );
+
+    SimdOp *dm_s = gr.make_op( "DIV", { di_a, gr.make_op( "SUB", { di_b, di_a } ) } );
+
+    SimdOp *aggx = gr.make_op( "AGG", {
+        gr.make_op( "GET 2", { px_0 } ),
+        gr.make_op( "GET 3", { px_0 } ),
+        gr.make_op( "ADD", { px_a, gr.make_op( "MUL", { dm_s, gr.make_op( "SUB", { px_a, px_b } ) } ) } ),
+        gr.make_op( "UNK", {} )
+    } );
+
+    // SimdVec<TF,2> nx_s = px_a + dm_s * ( px_a - px_b );
+
+    //    SimdOp *di_c = gr.make_op( "EXT 0", { di_0 } );
+    //    SimdOp *di_d = gr.make_op( "EXT 1", { di_0 } );
+
+    gr.add_target( aggx );
 
     gr.display();
 }
